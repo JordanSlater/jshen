87e5bb332cebe6f5f5ec2367823b648d7c923b93
Jordan Slater <slatjord@gmail.com>
2022-04-23 13:28:12 -0400

    Used git aliases

--- src/git.bash
+++ src/git.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
-alias n="nautilus"
-alias gs="git status "
-alias gf="git fetch "
-alias gd="git diff "
-alias gc="git checkout "
-alias gb="git branch "
-alias gp="git pull "
-alias glg="git log --graph --oneline"
-alias glgs="git log --graph --oneline -n 7"
-alias gbc="git checkout -b "
+alias g="git"
 
-alias gcam="git add . && git commit -m"
+git config --global alias.s status
+git config --global alias.c checkout
+git config --global alias.p pull
+git config --global alias.b branch
+git config --global alias.d diff
+git config --global alias.f fetch
+
+git config --global alias.unstage 'restore --staged'
+
+
+# detailed:
+git config --global alias.l "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(dim green)%an %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n      %C(bold black)%s%C(reset)'"
+
+# minimal/loner:
+git config --global alias.loner "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)%<(15)%ar%C(reset) %C(bold black)%s%C(auto)%+d%C(reset)'"
