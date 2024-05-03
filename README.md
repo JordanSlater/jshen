a6e74f340aa2ba5cea7588a70abfd81fa9769375
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2024-05-02 21:12:37 -0400

    Move to use folder

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usage
+Copy the bash source files you want to use to the `use` folder.
+
+Then source main in your `~/.bashrc` file. 
--- main.bash
+++ main.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JSHEN_DIR="$HOME/jshen/src"
+JSHEN_DIR="$HOME/jshen/use"
 for f in $(find $JSHEN_DIR -maxdepth 1 -name '*.bash' && find $JSHEN_DIR -mindepth 2 -name '*.bash'); do
     source $f;
     echo -n '.'
