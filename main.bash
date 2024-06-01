fc06ceed221d73680db32c999af67a63b8512b6a
JordanSlater <32441504+JordanSlater@users.noreply.github.com>
2024-05-31 20:27:13 -0400

    Add verbose mode to jshen for debugging

--- main.bash
+++ main.bash
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# JSHEN_VERBOSE=true
+
 JSHEN_DIR="$HOME/jshen/use"
+if [ "$JSHEN_VERBOSE" != true ]; then
+    echo -n '<'
+fi
+
 for f in $(find $JSHEN_DIR -maxdepth 1 -name '*.bash' && find $JSHEN_DIR -mindepth 2 -name '*.bash'); do
-    source "$f"
-    echo -n '.'
+    if [ "$JSHEN_VERBOSE" = true ]; then
+        echo -n "source $f"
+        time source "$f"
+        echo
+    else
+        source "$f"
+        echo -n '.'
+    fi
 done
-echo " Done."
+
+if [ "$JSHEN_VERBOSE" != true ]; then
+    echo -n '> '
+fi
+echo 'Done.'
