1e2bcf79a8de173b3dc619c82394703918c02770
Jordan Slater <32441504+JordanSlater@users.noreply.github.com>
2025-02-03 21:19:35 -0500

    Make git log more readable

--- src/git.bash
+++ src/git.bash
@@ -10,9 +10,9 @@ git config --global alias.d diff
 git config --global alias.f fetch
 
 # detailed:
-git config --global alias.l "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(dim green)%an %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n      %C(bold black)%s%C(reset)'"
+git config --global alias.l "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(dim green)%an %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n      %C(bold yellow)%s%C(reset)'"
 
 # minimal/loner:
-git config --global alias.loner "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)%<(15)%ar%C(reset) %C(bold black)%s%C(auto)%+d%C(reset)'"
+git config --global alias.loner "log --graph --decorate --abbrev-commit --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)%<(15)%ar%C(reset) %C(bold yellow)%s%C(auto)%+d%C(reset)'"
 
 alias gbh="git reflog show --format='%gs' | grep -oP 'checkout: moving from \K\S+'"
